eae6b3d457c7cd1089a1190ff559103610bdb714
QIhut <3423725215@qq.com>
2021-12-28 16:51:48 +0800

    beta value

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def run_optuna(path, dev):
         n_epoch = trial.suggest_int('n_epoch',140, 170, log=True)
         lr = trial.suggest_float('lr', 1e-4, 1e-3, log=True)
         au_weight = trial.suggest_float('au_weight', 0.001, 0.01, log=True)
-        beta = trial.suggest_float('beta', 0.1, 1, log=True)
+        beta = trial.suggest_float('beta', 1, 10, log=True)
         optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=2e-4)
         scheduler = WarmupMultiStepLR(optimizer, [90, 110], gamma=0.1, warmup_epochs=5)
         best_model_f1, best_epoch = dataset_train_contr(model, token, dataset, criterion, optimizer, n_epoch, au_weight, dev,
