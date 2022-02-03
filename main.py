2433d3579ebab297dff672f8150894490a5e50c0
QIhut <3423725215@qq.com>
2022-02-03 20:11:59 +0800

    revise imix params

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def main_run(params, path, dev):
     # n_epoch = 151
     # mix_w = 0.05
     # dataset = load_data(16, reverse=True, multi=True, mul_num=2400)
-    dataset = load_data(batch_size=16, radio=0.8)
+    dataset = load_data(batch_size=16, radio=0.2)
 
     optimizer = optim.SGD(model.parameters(), lr=params.lr, momentum=0.9, weight_decay=2e-4)
     scheduler = WarmupMultiStepLR(optimizer, [90, 110], gamma=0.1, warmup_epochs=5)
