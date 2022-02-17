ff81a8130c3f797a16ae8a590621cd1c0677dca5
QIhut <3423725215@qq.com>
2022-02-17 20:18:10 +0800

    cleaning code

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from transformers import AutoTokenizer, AutoConfig
 import torch.optim as optim
 from model.citation_model import *
 from model.citation_model_num import *
+from model.Model import *
 # from model.cnn_bert import *
 from utils.scheduler import WarmupMultiStepLR
-from train_valid.dataset_train import *
+# from train_valid.dataset_train import *
 from train_valid.dataset_valid import dataset_valid
+from train_valid.SupCNNTrain import *
 from utils.dataload import *
 from utils.util import *
 from sklearn.metrics import classification_report, confusion_matrix
@@ -38,6 +40,11 @@ def parse_args():
         default=None,
         type=str
     )
+    parser.add_argument(
+        "--epochs",
+        default=16,
+        type=int
+    )
     # parser.add_argument(
     #     '--radio',
     #     help="proportion of training data",
@@ -46,35 +53,55 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
+def set_optimizer(lr, model):
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = WarmupMultiStepLR(optimizer, [15, 25], gamma=0.1, warmup_epochs=5)
+    return optimizer, scheduler
+
+
+def set_model(tp, conf):
+    if tp == "supcnn":
+        cnnl, cnnr = CNN(768), CNN(768)
+        model = SupCNN('allenai/scibert_scivocab_uncased', config=conf, cnnl=cnnl, cnnr=cnnr)
+    elif tp == "onlysup":
+        model = OnlySupLoss('allenai/scibert_scivocab_uncased', config=conf)
+    else:
+        cnnl, cnnr = CNN(768), CNN(768)
+        model = OnlyCNN('allenai/scibert_scivocab_uncased', config=conf, cnnl=cnnl, cnnr=cnnr)
+    return model
+
+
+def set_token(dropout=0.3):
+    token = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+    conf = AutoConfig.from_pretrained('allenai/scibert_scivocab_uncased')
+    conf.hidden_dropout_prob = 0.3
+    criterion = nn.CrossEntropyLoss()
+    return token, criterion, conf
+
+
 def run_optuna(params, path, dev):
     print('Run optuna')
     setup_seed(0)
-    config = AutoConfig.from_pretrained('allenai/scibert_scivocab_uncased')
-    config.hidden_dropout_prob = 0.3
-    token = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
-    criterion = nn.CrossEntropyLoss()
+    token, criterion, conf = set_token()
+    # config = AutoConfig.from_pretrained('allenai/scibert_scivocab_uncased')
+    # config.hidden_dropout_prob = 0.3
+    # token = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+    # criterion = nn.CrossEntropyLoss()
     # dataset = load_data(16, reverse=True, multi=True, mul_num=2400)
-    dataset = load_data(params.dataname, batch_size=16, radio=0.2)
+    dataset = load_data(params.dataname, batch_size=params.epochs, radio=0.2)
 
     def objective(trial):
-        cnn1 = CNNBert(768)
-        cnn2 = CNNBert(768)
-        model = Model('allenai/scibert_scivocab_uncased', config=config, cnnl=cnn1, cnnr=cnn2)
-        # model = ModelCNN('allenai/scibert_scivocab_uncased', cnnl=cnn1, cnnr=cnn2)
-        # n_epoch = trial.suggest_int('n_epoch', 140, 170, log=True)
-        n_epoch = 35
+        model = set_model(params.tp, conf)
+        # n_epoch = 35
         lr = trial.suggest_float('lr', 1e-5, 1e-4, log=True)
-        au_weight = trial.suggest_float('au_weight', 0.001, 0.01, log=True)
-        # mix_w = trial.suggest_float('mix_w', 0.01, 0.1, log=True)
-        # beta = trial.suggest_float('beta', 1, 10, log=True)
+        auw = trial.suggest_float('au_weight', 0.001, 0.01, log=True)
         # optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=2e-4)
-        optimizer = optim.Adam(model.parameters(), lr=lr)
-        scheduler = WarmupMultiStepLR(optimizer, [15, 25], gamma=0.1, warmup_epochs=5)
-        # scheduler = None
-        # best_model_f1, best_epoch = dataset_train_imix(model, token, dataset, criterion, optimizer, n_epoch,
-        #                                                 au_weight, dev, mix_w, scheduler, model_path=path)
-        best_model_f1, best_epoch = dataset_train_suploss(model, token, dataset, criterion, optimizer, n_epoch,
-                                                          au_weight, dev, scheduler, model_path=path)
+        optimizer, scheduler = set_optimizer(lr, model=model)
+        # optimizer = optim.Adam(model.parameters(), lr=lr)
+        # scheduler = WarmupMultiStepLR(optimizer, [15, 25], gamma=0.1, warmup_epochs=5)
+        best_model_f1, best_epoch = onlycnn(model, token, dataset, criterion, optimizer, params.epochs,
+                                                          auw, dev, scheduler, model_path=path)
 
         return best_model_f1
     study = optuna.create_study(study_name='studyname', direction='maximize', storage='sqlite:///optuna.db',
@@ -86,42 +113,20 @@ def run_optuna(params, path, dev):
     print("Train".center(30, '-'))
     args.lr = float(format(study.best_params['lr'], '.6f'))
     args.au_weight = float(format(study.best_params['au_weight'], '.6f'))
-    if study.best_params.__contains__("mix_w"):
-
-        args.mix_w = float(format(study.best_params['mix_w'], '.6f'))
-    else:
-        args.mix_w = 0
     main_run(args, 'citation_mul_rev_model.pth', device)
 
 
 def main_run(params, path, dev):
     setup_seed(0)
-    token = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
-    config = AutoConfig.from_pretrained('allenai/scibert_scivocab_uncased')
-    config.hidden_dropout_prob = 0.3
-    # cnn1 = CNNBert(768)
-    # cnn2 = CNNBert(768)
-    # model = ModelCNN('allenai/scibert_scivocab_uncased', cnnl=cnn1, cnnr=cnn2)
-    cnn1 = CNNBert(768)
-    cnn2 = CNNBert(768)
-    model = Model('allenai/scibert_scivocab_uncased', config=config, cnnl=cnn1, cnnr=cnn2)
-    criterion = nn.CrossEntropyLoss()
-    n_epoch = 35
+    token, criterion, conf = set_token()
+    model = set_model(params.tp, conf)
+    # n_epoch = 35
     # lr = 0.0001
     # au_weight = 0.007413
-    # optimizer = SGD
-    # n_epoch = 151
-    # mix_w = 0.05
-    # dataset = load_data(16, reverse=True, multi=True, mul_num=2400)
-    dataset = load_data(params.dataname, batch_size=4, radio=0.8)
-
-    # optimizer = optim.SGD(model.parameters(), lr=params.lr, momentum=0.9, weight_decay=2e-4)
-    optimizer = optim.Adam(model.parameters(), lr=params.lr)
-    scheduler = WarmupMultiStepLR(optimizer, [15, 25], gamma=0.1, warmup_epochs=5)
-    # scheduler = None
-    best_model_f1, best_epoch = dataset_train_suploss(model, token, dataset, criterion, optimizer, n_epoch,
-                                                      params.au_weight, dev,
-                                                      scheduler, model_path=path)
+    dataset = load_data(params.dataname, batch_size=params.epochs, radio=0.8)
+    optimizer, scheduler = set_optimizer(params.lr, model)
+    best_model_f1, best_epoch = onlycnn(model, token, dataset, criterion, optimizer, params.epochs, params.auw, dev,
+                                        scheduler, model_path=path)
     print("best_model_f1:{} \t best_epoch:{}".format(best_model_f1, best_epoch))
     test_f1, test_micro_f1, test_true_label, test_pre_label = dataset_valid(model, token,
                                                                             dataset['test'], device,
@@ -136,7 +141,7 @@ def main_run(params, path, dev):
     generate_submission(test_pre, 'mul_rev_val_f1_{:.5}_best_epoch_{}'.format(best_model_f1, best_epoch), test_f1, params.dataname)
     c_matrix = confusion_matrix(test_true, test_pre, labels=[0, 1, 2, 3, 4, 5])
     per_eval = classification_report(test_true, test_pre, labels=[0, 1, 2, 3, 4, 5])
-    log_result(test_f1, best_model_f1,  c_matrix, per_eval, lr=params.lr, epoch=n_epoch, fun_name='main_multi_rev')
+    log_result(test_f1, best_model_f1,  c_matrix, per_eval, lr=params.lr, epoch=params.epochs, fun_name='main_multi_rev')
 # 是否在输出时加上 1- dropout, 不需要加 因为在训练阶段不仅会遮盖 还会将遮盖后的放大。  https://www.zhihu.com/question/61751133
 # 将SGD改为Adam
 
@@ -156,15 +161,15 @@ if __name__ == "__main__":
     # }
     args = parse_args()
     tst = time.time()
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
     if args.mode == 'optuna':
         run_optuna(args, 'citation_mul_rev_model.pth', device)
     else:
         with open('params.json', 'r', encoding='utf-8') as f:
             config = json.load(f)
         args.lr = config[args.dataname][args.tp]['lr']
-        args.au_weight = config[args.dataname][args.tp]['au_weight']
-        args.mix_w = config[args.dataname][args.tp]['mix_w']
+        args.auw = config[args.dataname][args.tp]['auw']
+        args.bsz = config[args.dataname][args.tp]['bsz']
+        args.epochs = config[args.dataname][args.tp]['epochs']
         main_run(args, 'citation_mul_rev_model.pth', device)
     ten = time.time()
     print('Total time: {}'.format((ten - tst)))
---
+++ model/Model.py
@@ -0,0 +1,360 @@
+"""
+SupLoss and CNN
+"""
+import torch 
+import torch.nn as nn
+from transformers import AutoModel
+import numpy as np
+import torch.nn.functional as F
+
+
+class CNN(nn.Module):
+    def __init__(self, emb_size):
+        super(CNN, self).__init__()
+        filter_sizes = [2, 3, 4]
+        num_filters = 4
+        self.conv1 = nn.ModuleList([nn.Conv2d(3, num_filters, (K, emb_size)) for K in filter_sizes])
+        self.dropout = nn.Dropout(0.1)
+        self.fc = nn.Linear(len(filter_sizes) * num_filters, 768)
+
+    def forward(self, bert_in):
+        x = (bert_in[7], bert_in[9], bert_in[12])
+        x = torch.stack(x, dim=1)
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.conv1]
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
+        x = torch.cat(x, 1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
+
+class SupCNN(nn.Model):
+    def __init__(self, model, config=None, cnnl=None, cnnr=None):
+        super(SupCNN, self).__init__()
+        self.encoder = AutoModel.from_pretrained(model, config)
+        self.drop = nn.Dropout(0.3)
+        self.cnnl = cnnl
+        self.cnnr = cnnr
+        self.fc1 = nn.Linear(768 * 4, 768)
+        self.fc2 = nn.Linear(768, 6)
+        self.afc1 = nn.Linear(768 * 2, 768)
+        self.afc2 = nn.Linear(768, 5)
+        self.supmlp = nn.Sequential(nn.Linear(768 * 2, 768), nn.ReLU(inplace=True), nn.Linear(768, 192))
+        self.ori_att = nn.Sequential(nn.Linear(768, 384), nn.Tanh(), nn.Linear(384, 1, bias=False))
+        self.re_att = nn.Sequential(nn.Linear(768, 384), nn.Tanh(), nn.Linear(384, 1, bias=False))
+
+    def forward(self, x1, **kwargs):
+        input_ids = x1['input_ids']
+        attention_mask = x1['attention_mask']
+        bert_output = self.model(input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        ori_sen_pre = self.get_sen_att(x1, bert_output, 'ori', attention_mask)
+        ocnn_sen_pre = self.cnnl(bert_output[2])
+        if self.training:
+            re_sen_pre, r_bert_out = self.generate_sen_pre(kwargs['r_sen'], 're')
+            rcnn_sen_pre = self.cnnr(r_bert_out[2])
+            au_sen_pre, a_bert_out = self.generate_sen_pre(kwargs['s_sen'], 'ori')
+            acnn_sen_pre = self.cnnl(a_bert_out[2])
+
+            ori_sen_pre = torch.cat((ori_sen_pre, ocnn_sen_pre), dim=1)
+            re_sen_pre = torch.cat((re_sen_pre, rcnn_sen_pre), dim=1)
+
+            mixed_feature = 2 * torch.cat((kwargs['l'] * ori_sen_pre, (1 - kwargs['l']) * re_sen_pre), dim=1)
+
+            sup_out1 = self.supmlp(ori_sen_pre)
+            sup_out1 = F.normalize(sup_out1, dim=1)
+
+            main_output = self.fc1(mixed_feature)
+            main_output = nn.ReLU(inplace=True)(main_output)
+            main_output = self.drop(main_output)
+            main_output = self.fc2(main_output)
+
+            au_sen_pre = torch.cat((au_sen_pre, acnn_sen_pre), dim=1)
+            au_output1 = self.au_task_fc1(au_sen_pre)
+            au_output1 = nn.ReLU(inplace=True)(au_output1)
+            au_output1 = self.drop(au_output1)
+            au_output1 = self.au_task_fc2(au_output1)
+            return main_output, au_output1, sup_out1
+
+        re_sen_pre = self.get_sen_att(x1, bert_output, 're', attention_mask)
+        rcnn_sen_pre = self.cnnr(bert_output[2])
+        ori_sen_pre = torch.cat((ori_sen_pre, ocnn_sen_pre), dim=1)
+        re_sen_pre = torch.cat((re_sen_pre, rcnn_sen_pre), dim=1)
+        feature = torch.cat((ori_sen_pre, re_sen_pre), dim=1)
+        feature = self.fc1(feature)
+        feature = nn.ReLU(inplace=True)(feature)
+        out = self.fc2(feature)
+        return out
+
+    def get_alpha(self, word_mat, data_type, mask):
+        if data_type == 'ori':
+            att_w = self.ori_att(word_mat)
+        else:
+            att_w = self.re_att(word_mat)
+        mask = mask.unsqueeze(2)
+        att_w = att_w.masked_fill(mask == 0, float('-inf'))
+        att_w = F.softmax(att_w, dim=1)
+        return att_w
+
+    def generate_sen_pre(self, sen, tp):
+        bert_output = self.model(sen['input_ids'], attention_mask=sen['attention_mask'], output_hidden_states=True)
+        sen = self.get_sen_att(sen, bert_output, tp, sen['attention_mask'])
+        return sen, bert_output
+
+    def get_word(self, sen, bert_output, mask):
+        input_t2n = sen['input_ids'].cpu().numpy()
+        sep_location = np.argwhere(input_t2n == 103)
+        sep_location = sep_location[:, -1]
+
+        # loc[0:size:2] 按步长取值
+
+        select_index = list(range(sen['length'][0]))
+        select_index.remove(0)  # 删除cls
+        lhs = bert_output.last_hidden_state
+        res = bert_output.hidden_states[8]
+        relength = []
+        recomposing = []
+        mask_recomposing = []
+        for i in range(lhs.shape[0]):
+            select_index_f = select_index.copy()
+            relength.append(sep_location[i] - 1)
+            select_index_f.remove(sep_location[i])
+            select_row = torch.index_select(lhs[i], 0,
+                                            index=torch.LongTensor(select_index_f).to(sen['input_ids'].device))
+            select_mask = torch.index_select(mask[i], 0,
+                                             index=torch.LongTensor(select_index_f).to(sen['input_ids'].device))
+            recomposing.append(select_row)
+            mask_recomposing.append(select_mask)
+        matrix = torch.stack(recomposing)
+        mask = torch.stack(mask_recomposing)
+        return matrix, mask
+
+    def get_sen_att(self, sen, bert_output, data_type, mask):
+        word_mat, select_mask = self.get_word(sen, bert_output, mask)
+        word_mat = self.drop(word_mat)
+        att_w = self.get_alpha(word_mat, data_type, select_mask)
+        word_mat = word_mat.permute(0, 2, 1)
+        sen_pre = torch.bmm(word_mat, att_w).squeeze(2)
+        return sen_pre
+
+
+class OnlySupLoss(nn.Module):
+    def __init__(self, model, config):
+        super(OnlySupLoss, self).__init__()
+        self.endocer = AutoModel.from_pretrained(model, config)
+        self.drop = nn.Dropout(0.3)
+
+        self.fc1 = nn.Linear(768 * 2, 768)
+        self.fc2 = nn.Linear(768, 6)
+
+        self.au_task_fc1 = nn.Linear(768, 768)
+        self.au_task_fc2 = nn.Linear(768, 5)
+
+        self.supmlp = nn.Sequential(nn.Linear(768, 768), nn.ReLU(inplace=True), nn.Linear(768, 192))
+        self.ori_att = nn.Sequential(nn.Linear(768, 384), nn.Tanh(), nn.Linear(384, 1, bias=False))
+        self.re_att = nn.Sequential(nn.Linear(768, 384), nn.Tanh(), nn.Linear(384, 1, bias=False))
+
+    def forward(self, x1, **kwargs):
+        input_ids = x1['input_ids']
+        attention_mask = x1['attention_mask']
+        bert_output = self.model(input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        ori_sen_pre = self.get_sen_att(x1, bert_output, 'ori', attention_mask)
+        if self.training:
+            re_sen_pre, r_bert_out = self.generate_sen_pre(kwargs['r_sen'], 're')
+            au_sen_pre, a_bert_out = self.generate_sen_pre(kwargs['s_sen'], 'ori')
+
+            mixed_feature = 2 * torch.cat((kwargs['l'] * ori_sen_pre, (1 - kwargs['l']) * re_sen_pre), dim=1)
+
+            sup_out1 = self.supmlp(ori_sen_pre)
+            sup_out1 = F.normalize(sup_out1, dim=1)
+
+            main_output = self.fc1(mixed_feature)
+            main_output = nn.ReLU(inplace=True)(main_output)
+            main_output = self.drop(main_output)
+            main_output = self.fc2(main_output)
+
+
+            au_output1 = self.au_task_fc1(au_sen_pre)
+            au_output1 = nn.ReLU(inplace=True)(au_output1)
+            au_output1 = self.drop(au_output1)
+            au_output1 = self.au_task_fc2(au_output1)
+            return main_output, au_output1, sup_out1
+
+        re_sen_pre = self.get_sen_att(x1, bert_output, 're', attention_mask)
+        feature = torch.cat((ori_sen_pre, re_sen_pre), dim=1)
+        feature = self.fc1(feature)
+        feature = nn.ReLU(inplace=True)(feature)
+        out = self.fc2(feature)
+        return out
+
+    def get_alpha(self, word_mat, data_type, mask):
+        if data_type == 'ori':
+            att_w = self.ori_att(word_mat)
+        else:
+            att_w = self.re_att(word_mat)
+        mask = mask.unsqueeze(2)
+        att_w = att_w.masked_fill(mask == 0, float('-inf'))
+        att_w = F.softmax(att_w, dim=1)
+        return att_w
+
+    def generate_sen_pre(self, sen, tp):
+        bert_output = self.model(sen['input_ids'], attention_mask=sen['attention_mask'], output_hidden_states=True)
+        sen = self.get_sen_att(sen, bert_output, tp, sen['attention_mask'])
+        return sen, bert_output
+
+    def get_word(self, sen, bert_output, mask):
+        input_t2n = sen['input_ids'].cpu().numpy()
+        sep_location = np.argwhere(input_t2n == 103)
+        sep_location = sep_location[:, -1]
+
+        # loc[0:size:2] 按步长取值
+
+        select_index = list(range(sen['length'][0]))
+        select_index.remove(0)  # 删除cls
+        lhs = bert_output.last_hidden_state
+        res = bert_output.hidden_states[8]
+        relength = []
+        recomposing = []
+        mask_recomposing = []
+        for i in range(lhs.shape[0]):
+            select_index_f = select_index.copy()
+            relength.append(sep_location[i] - 1)
+            select_index_f.remove(sep_location[i])
+            select_row = torch.index_select(lhs[i], 0,
+                                            index=torch.LongTensor(select_index_f).to(sen['input_ids'].device))
+            select_mask = torch.index_select(mask[i], 0,
+                                             index=torch.LongTensor(select_index_f).to(sen['input_ids'].device))
+            recomposing.append(select_row)
+            mask_recomposing.append(select_mask)
+        matrix = torch.stack(recomposing)
+        mask = torch.stack(mask_recomposing)
+        return matrix, mask
+
+    def get_sen_att(self, sen, bert_output, data_type, mask):
+        word_mat, select_mask = self.get_word(sen, bert_output, mask)
+        word_mat = self.drop(word_mat)
+        att_w = self.get_alpha(word_mat, data_type, select_mask)
+        word_mat = word_mat.permute(0, 2, 1)
+        sen_pre = torch.bmm(word_mat, att_w).squeeze(2)
+        return sen_pre
+
+
+class OnlyCNN(nn.Module):
+    def __init__(self, model, config, cnnl, cnnr):
+        super(OnlyCNN, self).__init__()
+        self.encoder = AutoModel.from_pretrained(model, config)
+        self.fc1 = nn.Linear(768 * 4, 768)
+        self.fc = nn.Linear(768, 6)
+        self.drop = nn.Dropout(0.3)
+        # self.au_task_fc1 = nn.Linear(768 * 2, 5)
+
+        self.afc1 = nn.Linear(768 * 2, 768)
+        self.afc2 = nn.Linear(768, 5)
+
+        self.ori_att = nn.Sequential(nn.Linear(768, 384), nn.Tanh(), nn.Linear(384, 1, bias=False))
+        self.re_att = nn.Sequential(nn.Linear(768, 384), nn.Tanh(), nn.Linear(384, 1, bias=False))
+
+        self.cnnl = cnnl
+        self.cnnr = cnnr
+
+    def forward(self, x1, **kwargs):
+        input_ids = x1['input_ids']
+        attention_mask = x1['attention_mask']
+        bert_output = self.model(input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        ori_sen_pre = self.get_sen_att(x1, bert_output, 'ori', attention_mask)
+        ocnn_out = self.cnnl(bert_output[2])
+        if self.training:
+            re_sen_pre, rb_output = self.generate_sen_pre(sen=kwargs['r_sen'], tp='re')
+            au_sen_pre, ab_output = self.generate_sen_pre(sen=kwargs['s_sen'], tp='ori')
+
+
+            rcnn_out = self.cnnr(rb_output[2])
+            acnn_out = self.cnnl(ab_output[2])
+
+            ori_sen_pre = torch.cat((ori_sen_pre, ocnn_out), dim=1)
+            re_sen_pre = torch.cat((re_sen_pre, rcnn_out), dim=1)
+            au_sen_pre = torch.cat((au_sen_pre, acnn_out), dim=1)
+
+            # ori_sen_pre = self.drop(ori_sen_pre)
+            # re_sen_pre = self.drop(re_sen_pre)
+
+            mixed_feature = 2 * torch.cat((kwargs['l'] * ori_sen_pre, (1 - kwargs['l']) * re_sen_pre), dim=1)
+
+            # main_output = self.fc1(self.drop(mixed_feature))
+            # main_output = self.fc(main_output)
+            main_output = self.fc1(mixed_feature)
+            main_output = nn.ReLU(inplace=True)(main_output)
+            main_output = self.drop(main_output)
+            main_output = self.fc2(main_output)
+
+            au_output1 = self.afc1(au_sen_pre)
+            au_output1 = nn.ReLU(inplace=True)(au_output1)
+            au_output1 = self.drop(au_output1)
+            au_output1 = self.afc2(au_output1)
+
+            # au_output1 = self.au_task_fc1(self.drop(ausec_sen_pre))
+            return main_output, au_output1
+        ori_sen_pre = torch.cat((ori_sen_pre, ocnn_out), dim=1)
+        rcnn_out = self.cnnr(bert_output[2])
+        re_sen_pre = self.get_sen_att(x1, bert_output, 're', attention_mask)
+        re_sen_pre = torch.cat((re_sen_pre, rcnn_out), dim=1)
+
+        feature = torch.cat((ori_sen_pre, re_sen_pre), dim=1)
+        feature = self.fc1(feature)
+        feature = nn.ReLU(inplace=True)(feature)
+        out = self.fc(feature)
+        return out
+
+    def generate_sen_pre(self, sen, tp):
+        bert_output = self.model(sen['input_ids'], attention_mask=sen['attention_mask'], output_hidden_states=True)
+        sen = self.get_sen_att(sen, bert_output, tp, sen['attention_mask'])
+        return sen, bert_output
+
+    def get_alpha(self, word_mat, data_type, mask):
+        if data_type == 'ori':
+            att_w = self.ori_att(word_mat)
+        else:
+            att_w = self.re_att(word_mat)
+
+        mask = mask.unsqueeze(2)
+        att_w = att_w.masked_fill(mask == 0, float('-inf'))
+        att_w = F.softmax(att_w, dim=1)
+        return att_w
+
+    # Get useful words vectors
+    def get_word(self, sen, bert_output, mask):
+        input_t2n = sen['input_ids'].cpu().numpy()
+        sep_location = np.argwhere(input_t2n == 103)
+        sep_location = sep_location[:, -1]
+
+        # loc[0:size:2] 按步长取值
+
+        select_index = list(range(sen['length'][0]))
+        select_index.remove(0)  # 删除cls
+        lhs = bert_output.last_hidden_state
+        res = bert_output.hidden_states[8]
+        relength = []
+        recomposing = []
+        mask_recomposing = []
+        for i in range(lhs.shape[0]):
+            select_index_f = select_index.copy()
+            relength.append(sep_location[i] - 1)
+            select_index_f.remove(sep_location[i])
+            select_row = torch.index_select(lhs[i], 0,
+                                            index=torch.LongTensor(select_index_f).to(sen['input_ids'].device))
+            select_mask = torch.index_select(mask[i], 0,
+                                             index=torch.LongTensor(select_index_f).to(sen['input_ids'].device))
+            recomposing.append(select_row)
+            mask_recomposing.append(select_mask)
+        matrix = torch.stack(recomposing)
+        mask = torch.stack(mask_recomposing)
+        return matrix, mask
+
+    # Get the representation vector after calculating the attention mechanism
+    def get_sen_att(self, sen, bert_output, data_type, mask):
+        word_mat, select_mask = self.get_word(sen, bert_output, mask)
+        word_mat = self.drop(word_mat)
+        att_w = self.get_alpha(word_mat, data_type, select_mask)
+        word_mat = word_mat.permute(0, 2, 1)
+        sen_pre = torch.bmm(word_mat, att_w).squeeze(2)
+        return sen_pre
+
+
